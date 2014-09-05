e6b91146b8641176819ef382ec4dd93eb33d7eba
matanster <dev.matan@gmail.com>
2014-09-06 01:39:58 +0300

    ""

--- src/main/scala/com/articlio/Server.scala
+++ src/main/scala/com/articlio/Server.scala
@@ -12,6 +12,22 @@ import scala.io.Source
 import org.ahocorasick.trie._
 import scala.collection.JavaConversions._
 
+class debugger {
+  import scala.tools.nsc.interpreter.ILoop
+  import scala.tools.nsc.interpreter.SimpleReader
+  import scala.tools.nsc.Settings  
+
+  val repl = new ILoop
+  repl.settings = new Settings
+  repl.settings.Yreplsync.value = true
+  repl.in = SimpleReader()
+  repl.createInterpreter()
+
+  // start the interpreter and then close it after you :quit
+  repl.loop()
+  repl.closeInterpreter()
+}
+
 class deployer extends Verticle {
   override def start() {
     container.logger.info("starting deployer verticle")
@@ -29,7 +45,10 @@ class ldb extends Verticle {
     val emits = trie.parseText("sugar cane sugarcane sugar canesugar")
     emits.foreach(println)
 
-
+    def f[T](v: T) = v
+    val t = emits.getClass
+    println(t)
+  
     //container.logger.info("starting linguistic database verticle")
     System.out.println("starting linguistic database verticle")
     val databaseInputFile = "/home/matan/Downloads/database-machine-ready July 24 2014 non-abstract database - Markers.csv"
