dd0f31b6a3dfe8cf5a199062947cedc24393155c
matanster <dev.matan@gmail.com>
2014-09-27 13:18:14 +0300

    "logging enhancements"

--- src/main/scala/com/articlio/util/console.scala
+++ src/main/scala/com/articlio/util/console.scala
@@ -2,9 +2,9 @@ package com.articlio.util
 
 object Console {
 
-  import scala.Console._     // Available colors and styling at: http://www.scala-lang.org/api/2.10.2/index.html#scala.Console$
-  val GRAY    = "\033[90m"   // And some more color
-  val ITALICS = "\033[3m"    // And some more style
+  import scala.Console._     // available colors and styling at: http://www.scala-lang.org/api/2.10.2/index.html#scala.Console$
+  val GRAY    = "\033[90m"   // and some more 
+  val ITALICS = "\033[3m"    // and some more 
 
   def log(message:String, msgType: String) {
 
